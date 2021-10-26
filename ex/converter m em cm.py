4f13dc738daea7834a9f2e7642b0b4131ff876b0
Dani <daniele.ddsp@gmail.com>
2021-10-26 00:02:51 -0300

    Create converter m em cm.py

---
+++ ex/converter m em cm.py
@@ -0,0 +1,6 @@
+# converter metros em centímetros
+# lembrando que 1m = 1000cm
+
+m = float (input("insira quantidade de metros pra serem convertidos para centímetros"))
+cm = float(m) * int(1000)
+print(f"temos {m}m convertido em {cm}cm")
