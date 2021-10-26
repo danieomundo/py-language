2a6c1e3a0d7457eced0ab17f1c5a413a847b1a0c
Dani <daniele.ddsp@gmail.com>
2021-10-26 00:03:33 -0300

    Create converter temperatura.py

---
+++ ex/converter temperatura.py
@@ -0,0 +1,8 @@
+''' Faça um programa que transforme uma temperatura fornecida em Celsius para a correspondente em Fahrenheit. 
+A fórmula de conversão de Celsius para Fahrenheit é a seguinte: C = (5/9) * (F –32). '''
+
+tempF = float(input("digite a temperatura que deseja converter"))
+
+C = (5/9) * (tempF - 32)
+
+print(C)
